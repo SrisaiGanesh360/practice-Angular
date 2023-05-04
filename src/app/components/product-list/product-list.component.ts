953083bb9f47a110be45e378291632dc09057c89
Ganesh99 <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-05-04 17:39:07 +0530

    sweetalert

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as data from './products_data.json';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-product-list',
@@ -10,4 +11,7 @@ export class ProductListComponent {
   products = (data as any).default;
   p: number = 1;
   searchText: string = '';
+  addToCart(){
+    Swal.fire('Congratulations!', "You're item is added to cart", 'success');
+  }
 }
