846e9b7872783fda306ceb05921e4762858a0844
Ganesh99 <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-05-07 18:51:25 +0530

    remaining pipe explanation

--- src/app/custom-pipes/remaining.pipe.ts
+++ src/app/custom-pipes/remaining.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'remaining',
 })
 export class RemainingPipe implements PipeTransform {
-  transform(msg: string, maxChar: number) {
+  transform(msg: string, maxChar: number) { // maxChar is the argument that takes remaining : 100 value and that 100 can be changed based on requirement
     return maxChar - msg.length;
   }
 }
