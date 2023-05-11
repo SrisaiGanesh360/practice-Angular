23f36395bfcc7a47eaff4cf96dcc184452cf5b10
T Srisai Ganesh <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-05-11 22:02:52 +0530

    Update highlight.directive.ts
    Highlight directive update

--- src/app/custom-directves/highlight.directive.ts
+++ src/app/custom-directves/highlight.directive.ts
@@ -8,12 +8,16 @@ export class HighlightDirective {
   constructor(private ele: ElementRef) {
     console.log(ele);
   }
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('grey', 'white');
+  
+@HostListener('mouseenter') onMouseEnter() {
+    this.ele.nativeElement.style.transform = 'rotateY(180deg)';
+    this.ele.nativeElement.style.transition = '3s';
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('', '');
+    this.ele.nativeElement.style.transform = 'rotateY(180deg)';
+    this.ele.nativeElement.style.transition = '5s';
   }
+
   private highlight(bgColor: string, color: string) {
     this.ele.nativeElement.style.backgroundColor = bgColor;
     this.ele.nativeElement.style.color = color;
