924d73dec953b26a6e76c70b6e596ed6387d64b6
Ganesh99 <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-06-23 19:27:13 +0530

    services

--- src/app/components/mathdemo1/mathdemo1.component.ts
+++ src/app/components/mathdemo1/mathdemo1.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MathService } from 'src/app/services/math.service';
 
 @Component({
   selector: 'app-mathdemo1',
   templateUrl: './mathdemo1.component.html',
-  styleUrls: ['./mathdemo1.component.css']
+  styleUrls: ['./mathdemo1.component.css'],
 })
 export class Mathdemo1Component {
+  a = 10;
+  b = 20;
+  addResult: number = 0;
 
+
+  constructor(private mathService: MathService) {
+    // dependency injection
+  }
+  ngonInit(): void {
+    this.addResult = this.mathService.add(this.a, this.b);
+    console.log(`Addition of ${this.a} & ${this.b} is ${this.addResult} `);
+  }
 }
